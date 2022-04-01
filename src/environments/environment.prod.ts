899ead08b63a0aac4daaacc1b68fa0d76b11514f
Kaczynski <kaczynss@emea.roche.com>
2022-04-01 10:17:21 +0200

    Add firebase and home-page component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'jiraffa-93ce4',
+    appId: '1:2878889580:web:1b963a86e1fa0fc59dc249',
+    storageBucket: 'jiraffa-93ce4.appspot.com',
+    apiKey: 'AIzaSyD5zR2w9qU95xAj4ar-oAOJ_K0-61qPi1I',
+    authDomain: 'jiraffa-93ce4.firebaseapp.com',
+    messagingSenderId: '2878889580',
+    measurementId: 'G-QGT7Z7NXG7',
+  }
 };
