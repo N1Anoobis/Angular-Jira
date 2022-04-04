ff164a05628a90d41c19ecf916ae452e540d62b5
Kaczynski <kaczynss@emea.roche.com>
2022-04-04 12:16:16 +0200

    Add Apollo-Angular

--- myspace/apps/nx-apollo/src/app/app.component.ts
+++ myspace/apps/nx-apollo/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { InMemoryCache } from '@apollo/client/cache/inmemory/inMemoryCache';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'myspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'nx-apollo';
+export class AppComponent implements OnInit, DoCheck {
+  rates!: any[];
+  loading = true;
+  error: any;
+  cache!: InMemoryCache;
+  reactiveVar: any;
+
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result?.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+
+      this.cache = new InMemoryCache({})
+  }
+
+  ngDoCheck() {
+    console.log(this.apollo)
+  }
 }
--- myspace/apps/nx-apollo/src/app/app.module.ts
+++ myspace/apps/nx-apollo/src/app/app.module.ts
@@ -3,11 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, ApolloModule, HttpClientModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io',         
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
